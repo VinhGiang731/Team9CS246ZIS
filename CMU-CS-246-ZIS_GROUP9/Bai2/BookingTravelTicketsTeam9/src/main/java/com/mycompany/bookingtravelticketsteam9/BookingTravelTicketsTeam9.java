19ee0c2ba48e08cf35cc49729b0c75dcb4135e89
nam le <namsle2004@gmail.com>
2024-04-04 20:33:51 +0700

    BookIng

---
+++ CMU-CS-246-ZIS_GROUP9/Bai2/BookingTravelTicketsTeam9/src/main/java/com/mycompany/bookingtravelticketsteam9/BookingTravelTicketsTeam9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bookingtravelticketsteam9;
+
+/**
+ *
+ * @author HI
+ */
+public class BookingTravelTicketsTeam9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
