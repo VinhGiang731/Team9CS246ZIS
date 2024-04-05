f2185a81d24d5b2ea50dec9c632c6c452957607a
nam le <namsle2004@gmail.com>
2024-04-06 01:55:38 +0700

    Update1

---
+++ CMU-CS-246-ZIS_GROUP9/Bai2/BookingTravelTicketsTeam9/src/main/java/BookingTicket/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BookingTicket;
+
+/**
+ *
+ * @author HI
+ */
+public class Main {
+    public static void main(String[] args) {
+     Booking B = new Booking();
+     B.pack();
+     B.setLocationRelativeTo(null);
+     B.setDefaultCloseOperation(B.EXIT_ON_CLOSE);
+     B.setVisible(true);
+    }
+
+}
