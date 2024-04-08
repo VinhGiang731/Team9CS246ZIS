37a2c41a4de981dad3a010cecf9913be597951b8
Nguyen Vy <nguyenvydn2004@gmail.com>
2024-04-08 19:48:20 +0700

    nguyen vy update

---
+++ CMU-CS-246-ZIS_GROUP9/Bai2_NguyenVy/BookingTravelTicketsTeam9/src/main/java/com/mycompany/bookingtravelticketsteam9/BookingTravelTicketsTeam9.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bookingtravelticketsteam9;
+
+import java.awt.print.Book;
+
+/**
+ *
+ * @author HI
+ */
+public class BookingTravelTicketsTeam9 {
+
+    public static void main(String[] args) {
+        Book m = new Book();
+    }
+}
