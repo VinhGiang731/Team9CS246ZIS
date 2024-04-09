6e4dca9ee8a7b0831bd91943d46a63c1dc55e800
VinhGiang731 <dinhvinhgiang345@gmail.com>
2024-04-09 23:41:21 +0700

    Giangupdate

---
+++ CMU-CS-246-ZIS_GROUP9/Bai2_NguyenVy/BookingTravelTicketsTeam9/src/main/java/diaDiemDen.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class diaDiemDen {
+    static ArrayList<String> l = new ArrayList<>();
+    
+    public static ArrayList<String> getdiaDiemDen(){
+        l.add(" Hồ Chí Minh ");
+        l.add(" Đà Nẵng ");
+        l.add(" Hà Nội ");
+        l.add(" Hải Phòng ");
+        l.add(" Đà Lạt ");
+        return l;
+    }
+}
